de2429b26c2e2a0129cf28765bd86dcbd4a51a80
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-07-11 13:19:48 -0400

    first commit consructor created

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      
+    }
+  }
   render() {
     return (
       <div>
