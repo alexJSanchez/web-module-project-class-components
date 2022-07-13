4f2db33be586f8bb630aa43a996155de0b1b55bb
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-07-13 15:51:11 -0400

    set handleClear on button

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,36 +1,40 @@
-import React from 'react'
-import TodoList from './TodoList';
+import React from "react";
+import TodoList from "./TodoList";
+import Form from "./Form";
 
 export default class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state = {      
+
+    this.state = {
       todolist: [
         {
-          name: 'Organize Garage',
+          name: "Organize Garage",
           id: 1528817077286, // could look different, you could use a timestamp to generate it
-          completed: false
+          completed: false,
         },
         {
-          name: 'Bake Cookies',
+          name: "Bake Cookies",
           id: 1528817084358,
-          completed: false
-        }
+          completed: true,
+        },
       ],
-    }
+    };
+  }
+
+  handleClear = (e) => { 
+    e.preventDefault
+   this.setState({...this.state, todolist: this.state.todolist.filter(todo => {return (todo.completed===false)})}) 
   }
   render() {
-    const {todolist} = this.state;
+    const { todolist } = this.state;
     return (
-      <div>
-      <h1>Todos:</h1>
-     <TodoList todo={todolist}/>  
-        <form>
-          <input/>
-          <button>Add</button>
-          <button>Clear</button>
-        </form>
-      </div>
-    )
+      <>
+        <h1>Todos:</h1>
+        <TodoList todo={todolist} />
+        <Form />
+        <button onClick={this.handleClear}>Clear</button>
+      </>
+    );
   }
 }
